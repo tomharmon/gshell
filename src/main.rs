40f941e6ca337447cf2eac55cbcf4c7b40c495a6
thomas harmon <thomas.alan.harmon@gmail.com>
2019-09-08 16:18:29 -0400

    splitting stdin on whitespace in a loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use std::io::{self, Write};
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+        print!("gshell$");
+        io::stdout().flush().expect("couldn't print command prompt");
+
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("failed to read line");
+
+        for s in input.split_ascii_whitespace() {
+            println!("{}", s);
+        }
+    }
 }
