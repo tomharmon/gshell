481e4b62211632d11c6535bb0d91a694daa526c2
thomas harmon <thomas.alan.harmon@gmail.com>
2019-10-15 22:29:19 -0400

    Fixed compile warnings and formatted

--- src/ast.rs
+++ src/ast.rs
@@ -1,14 +1,12 @@
-use std::fs::File;
-use std::os::unix::io::{AsRawFd};
 use std::io::{stdin, stdout};
-use std::process::{Command, exit};
+use std::os::unix::io::AsRawFd;
+use std::process::{exit, Command};
 use std::thread;
 
-
-use nix::unistd::{ForkResult, fork, close, dup, pipe};
-use nix::sys::wait::{ waitpid, WaitStatus };
-use nix::sys::stat::Mode;
 use nix::fcntl::{open, OFlag};
+use nix::sys::stat::Mode;
+use nix::sys::wait::waitpid;
+use nix::unistd::{close, dup, fork, pipe, ForkResult};
 
 use super::enums::Op;
 
@@ -18,6 +16,7 @@ pub enum Ast {
     Leaf(String, Vec<String>),
 }
 
+// return Result (so we can use `?` everywhere instead of unwrap), or Option?
 pub fn eval_ast(tree: Box<Option<Ast>>) -> Option<i32> {
     match *tree {
         Some(Ast::Leaf(c, args)) => {
@@ -54,55 +53,59 @@ pub fn eval_ast(tree: Box<Option<Ast>>) -> Option<i32> {
         // check for redirect in <
         Some(Ast::Node(left_child, right_child, Op::RedirectIn)) => {
             match fork() {
-                Ok(ForkResult::Parent {child, ..}) => {
+                Ok(ForkResult::Parent { child, .. }) => {
                     match waitpid(child, None) {
-                        Ok(x) => Some(5), // uhhh
-                        Err(e) => None // uhhhh
+                        Ok(_) => Some(5), // uhhh
+                        Err(_) => None,   // uhhhh
                     }
-                },
+                }
                 Ok(ForkResult::Child) => {
                     match *right_child {
-                        Some(Ast::Leaf(fileName, _)) => {
-                            let file = open(fileName.as_str(), OFlag::O_RDONLY, Mode::all()).unwrap();
-                            close(stdin().as_raw_fd());
-                            dup(file);
-                            close(file);
+                        Some(Ast::Leaf(file_name, _)) => {
+                            // TODO: actual error handling for opening file, modes?
+                            let file = open(file_name.as_str(), OFlag::O_RDONLY, Mode::all())
+                                .expect("could not open file to read in");
+                            // i think these unwraps are fine, can panic in this case
+                            close(stdin().as_raw_fd()).unwrap();
+                            dup(file).unwrap();
+                            close(file).unwrap();
                             eval_ast(left_child);
                             exit(1)
                         }
-                        _ => { panic!("need a file after <"); }
+                        _ => {
+                            panic!("need a file after <");
+                        } // TODO: this already never happens bc of parser iirc, right
                     }
-                },
-                Err(e)=> None // uhh
+                }
+                Err(_) => None, // uhh
             }
         }
         // check for redirect out >
         Some(Ast::Node(left_child, right_child, Op::RedirectOut)) => {
             match fork() {
-                Ok(ForkResult::Parent {child, ..}) => {
+                Ok(ForkResult::Parent { child, .. }) => {
                     match waitpid(child, None) {
-                        Ok(x) => {
-                            return Some(5); // uhh
-                        },
-                        Err(e) => None // uhh
+                        Ok(_) => Some(5), // uhh
+                        Err(_) => None,   // uhh
                     }
-                },
+                }
                 Ok(ForkResult::Child) => {
                     match *right_child {
-                        Some(Ast::Leaf(fileName, _)) => {
-                            let file = open(fileName.as_str(), OFlag::O_CREAT | OFlag::O_WRONLY, Mode::all()).unwrap();
-                            close(stdout().as_raw_fd());
-                            dup(file);
-                            close(file);
-                            eval_ast(left_child);
+                        Some(Ast::Leaf(file_name, _)) => {
+                            // TODO: actual error handling for creating file, def fix modes
+                            let file = open(file_name.as_str(), OFlag::O_CREAT | OFlag::O_WRONLY, Mode::all()).unwrap();
+                            close(stdout().as_raw_fd()).unwrap();
+                            dup(file).unwrap();
+                            close(file).unwrap();
+                            eval_ast(left_child).unwrap();
                             exit(1);
                         }
-                        _ => { panic!("need a file after >") }
+                        _ => panic!("need a file after >"), // TODO: this already never happens bc of parser iirc, right
                     }
-                },
-                Err(e)=> None // uhh
+                }
+                Err(_) => None, // uhh
             }
-        },
+        }
         // check for &&
         Some(Ast::Node(left_child, right_child, Op::And)) => {
             let left_rv = eval_ast(left_child);
@@ -138,35 +141,34 @@ pub fn eval_ast(tree: Box<Option<Ast>>) -> Option<i32> {
         // check for |
         Some(Ast::Node(left_child, right_child, Op::Pipe)) => {
             match fork() {
-                Ok(ForkResult::Parent {child, ..}) => {
+                Ok(ForkResult::Parent { child, .. }) => {
                     match waitpid(child, None) {
-                        Ok(x) => Some(5), // uhh
-                        Err(e) => None // uhh
+                        Ok(_) => Some(5), // uhh
+                        Err(_) => None,   // uhh
                     }
-                },
+                }
                 Ok(ForkResult::Child) => {
-                   let p = pipe().unwrap();
-                   match fork() {
-                        Ok(ForkResult::Parent {child, ..}) => { 
-                            close(stdin().as_raw_fd());
-                            dup(p.0);
-                            close(p.1);
-                            eval_ast(right_child);
-                            waitpid(child, None);
+                    let p = pipe().unwrap();
+                    match fork() {
+                        Ok(ForkResult::Parent { child, .. }) => {
+                            close(stdin().as_raw_fd()).unwrap();
+                            dup(p.0).unwrap();
+                            close(p.1).unwrap();
+                            eval_ast(right_child); // TODO: what to do w returned val?
+                            let _ = waitpid(child, None); // TODO: what to do w return val
                             exit(1);
-                        },
-                        Ok(ForkResult::Child) => { 
-                            close(stdout().as_raw_fd());
-                            dup(p.1);
-                            close(p.0);
+                        }
+                        Ok(ForkResult::Child) => {
+                            close(stdout().as_raw_fd()).unwrap();
+                            dup(p.1).unwrap();
+                            close(p.0).unwrap();
                             eval_ast(left_child);
                             exit(1);
-                        },
-                        Err(e)=> None // uhh 
-                   }
-
-                },
-                Err(e)=> None // uhh
+                        }
+                        Err(_) => None, // uhh
+                    }
+                }
+                Err(_) => None, // uhh
             }
         }
         _ => None,
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
-use std::io::{self, Write, Read};
-use std::os::unix::io::AsRawFd;
-use std::fs::File;
 use std::env;
+use std::fs::File;
+use std::io::{self, Read, Write};
 
 mod ast;
 mod enums;
@@ -9,28 +8,32 @@ mod lexer;
 mod parser;
 
 fn main() {
-    // let args: Vec<String> = env::args().collect();
-    // // match args.get(1) {
-    // //     Some(s) => {
-    // //         let mut file = File::open(s).unwrap();
-    // //         let mut input = String::new();
-    // //         file.read_to_string(&mut input);
-    // //         let mut tokens: Vec<enums::Token> = Vec::new();
-    // //         lexer::tokenize(&mut input, &mut tokens);
-
-    // //         let ast = parser::make_ast(&tokens);
-    // //         match ast {
-    // //             Ok(ast) => {
-    // //                 ast::eval_ast(ast, AsRawFd::as_raw_fd(&io::stdin()), AsRawFd::as_raw_fd(&io::stdout()));
-    // //             }
-    // //             Err(message) => {
-    // //                 println!("{}", message);
-    // //             }
-    // //         }
-    // //         return;
-    // //     }
-    // //     _ => {}
-    // // }
+    let args: Vec<String> = env::args().collect();
+    match args.get(1) {
+        Some(s) => {
+            let open_file_result = File::open(s);
+            match open_file_result {
+                Ok(mut file) => {
+                    let mut input = String::new();
+                    let _ = file.read_to_string(&mut input).unwrap();
+                    let mut tokens: Vec<enums::Token> = Vec::new();
+                    lexer::tokenize(&mut input, &mut tokens);
+                    let ast = parser::make_ast(&tokens);
+                    match ast {
+                        Ok(ast) => {
+                            ast::eval_ast(ast);
+                        }
+                        Err(message) => {
+                            println!("{}", message);
+                        }
+                    }
+                }
+                Err(_) => println!("Could not open file: {}", s),
+            }
+            return;
+        }
+        _ => (),
+    }
 
     loop {
         print!("gshell$");
